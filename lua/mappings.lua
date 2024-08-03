f7419b74d805661a8f1b1e10e789665760cbcb23
eweix <elliott.weix@icloud.com>
2024-08-03 11:05:42 -0500

    update plugins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,13 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- mapping for ProjectMgr
+map("n", "<leader>fp", "<CMD> ProjectMgr<CR>", { desc = "open projects" })
+
+-- mappings for iron REPL
+map("n", "<space>rs", "<cmd>IronRepl<cr>", { desc = "open REPL" })
+map("n", "<space>rr", "<cmd>IronRestart<cr>", { desc = "restart REPL" })
+map("n", "<space>rf", "<cmd>IronFocus<cr>", { desc = "focus REPL" })
+-- map("n", "<space>rh", "<cmd>IronHide<cr>", { desc = "hide REPL" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,8 @@
 return {
+
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -26,22 +27,17 @@ return {
       require "configs.iron.keys"
     end,
     config = function()
-      local iron = require("iron.core")
+      local iron = require "iron.core"
       local options = require "configs.iron.options"
       iron.setup(options)
       -- iron also has a list of commands, see :h iron-commands for all available commands
-      vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-      vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-      vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
-      vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
     end,
   },
 
-
   {
     "nvim-orgmode/orgmode",
-    event = 'VeryLazy',
-    ft = { 'org' },
+    event = "VeryLazy",
+    ft = { "org" },
     config = function()
       require "configs.orgmode"
     end,
@@ -57,24 +53,30 @@ return {
 
   {
     "williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
-  		},
-  	},
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+      },
+    },
   },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css",
-       "python", "scheme",
-  		},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+        "scheme",
+      },
       ignore_install = { "org" },
-  	},
+    },
   },
-
 }
